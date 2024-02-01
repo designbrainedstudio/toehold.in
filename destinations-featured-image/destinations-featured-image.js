64533777ca8feeb844cd1c8c0223d1cd854616cf
Hemanth Sharma <hi@designbrained.studio>
2024-02-01 14:15:15 +0530

    Added - Destinations featured image component.

---
+++ destinations-featured-image/destinations-featured-image.js
@@ -0,0 +1,11 @@
+// Function to replace the entire element based on its content
+function replaceDestinationFeaturedImage() {
+    // Iterate through elements with class "data_destination-featured-image"
+    $(".data_destination-featured-image").each(function() {
+      // Get the content of each element
+      var imageData = $(this).text().trim();
+      
+      // Replace with <img> tag
+      $(this).replaceWith('<img src="' + imageData + '" class="destinations-page_dest-card-bg-image">');
+    });
+}
