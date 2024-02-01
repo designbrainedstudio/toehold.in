ff7d8238f8c2a443cde8c0dc7d90de4498bd66c0
Hemanth Sharma <hi@designbrained.studio>
2024-02-01 11:28:04 +0530

    Add - Tour Icon Replace Function.

---
+++ tour-icon-component/tour-icon-mapping.js
@@ -0,0 +1,20 @@
+// Function to replace the entire element based on JSON data
+function replaceTourIconElements() {
+	var jsonData = defineTourIcons();
+
+      // Iterate through elements with class "tour-icon-data"
+      $(".tour-icon-data").each(function() {
+        // Get the content of each element
+        var iconData = $(this).text().trim();
+
+        // Find the corresponding object in JSON data
+        var matchingIcon = jsonData.find(function(icon) {
+          return icon.tourIconName === iconData;
+        });
+
+        // If a match is found, replace with <img> tag
+        if (matchingIcon) {
+          $(this).replaceWith('<img src="' + matchingIcon.tourIconFile + '" class="tour-icon">');
+        }
+      });
+}
