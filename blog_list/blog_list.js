bd3692d6903dd01537055d521e1a0eb4d177072e
Hemanth Sharma <hi@designbrained.studio>
2024-02-08 08:05:51 +0530

    Added Blog List Thumbanails.

---
+++ blog_list/blog_list.js
@@ -0,0 +1,12 @@
+// Function to replace the entire element based on its content
+function replaceBlogListThumbnails() {
+    // Iterate through elements with class "data_blog-thumbnail"
+    $(".data_blog-thumbnail").each(function() {
+      // Get the content of each element
+      var imageData = $(this).text().trim();
+      
+      // Replace with <img> tag
+      $(this).replaceWith('<img src="' + imageData + '" class="blog1_image">');
+    });
+}
+
