d98ced45f8ca111d24ecb574a03415a7cb4a957c
Hemanth Sharma <hi@designbrained.studio>
2024-02-08 07:25:44 +0530

    Adding Itinerary Hero Image & Blog Single Share Buttons.

---
+++ itinerary_hero-image/itinerary_hero-image.js
@@ -0,0 +1,12 @@
+// Function to replace the entire element based on its content
+function replaceItineraryHeroImage() {
+    // Iterate through elements with class "data_hero-image"
+    $(".data_hero-image").each(function() {
+      // Get the content of each element
+      var imageData = $(this).text().trim();
+      
+      // Replace with <img> tag
+      $(this).replaceWith('<img src="' + imageData + '" class="itinerary_hero-image">');
+    });
+}
+
