a28e32ee1275a245f918e2f47a777b7382cee8df
Hemanth Sharma <hi@designbrained.studio>
2024-03-18 17:03:00 +0530

    Fixed home dest thumb replacer.

--- home/home.js
+++ home/home.js
@@ -3,12 +3,14 @@
   ********************************************************************************************* */
 // Function to replace the entire element based on its content
 function replaceDestinationThumbnails() {
-    // Iterate through elements with class "data_destination-thumbnail"
-    $(".data_destination-thumbnail").each(function() {
-      // Get the content of each element
-      var imageData = $(this).text().trim();
-  
-      // Replace with <img> tag
-      $(this).replaceWith('<img src="' + iconData + '" class="generic_destination-card-thumbnail">');
-    });
-  }
+  // Iterate through elements with class "data_destination-thumbnail"
+  $(".data_destination-thumbnail").each(function () {
+    // Get the content of each element
+    var imageData = $(this).text().trim();
+
+    // Replace with <img> tag
+    $(this).replaceWith(
+      '<img src="' + imageData + '" class="generic_destination-card-thumbnail">'
+    );
+  });
+}
